d3039aedd9d549044090ffebcd31ded681229c2e
Kibetmutai <kibetmutagil738@gmail.com>
2019-06-25 08:39:29 +0300

    add /api/customer

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 
-const express = request("express");
+const express = require("express");
 const mysql = require("mysql");
 const app = express();
 const pool = mysql.createPool({
     host: "localhost",
     user: "foo",
     password: "password",
-    database: "house_management_system"
+    database: "house_management"
 });
 
-app.get("/api/customer", (req,res)=>{
+app.get("/api/customer", (req, res) => {
     pool.query("SELECT customer_id, name, email, phone_no, image, distription FROM customer ", (error, rows) => {
-        if (error){
-            return res.status(500).json({error});
+        if (error) {
+            return res.status(500).json({ error });
         }
 
         res.json(rows);
