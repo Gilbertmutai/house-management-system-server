011c7aff1816967b5b956d79412ced232790ca52
Kibetmutai <kibetmutaigil738@gmail.com>
2019-06-27 19:05:54 +0300

    add env-sample

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-
+require("dotenv").config();
 const express = require("express");
 const mysql = require("mysql");
 const app = express();
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "foo",
-    password: "password",
-    database: "house_management"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
 app.get("/api/customer", (req, res) => {
