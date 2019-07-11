ead77920c04eb9ddd1ae0434c2f77a463e12d181
Kibetmutai <kibetmutaigil738@gmail.com>
2019-07-11 09:49:58 +0300

    api/customer/:id

--- index.js
+++ index.js
@@ -96,12 +96,12 @@ app.post("/api/customer", (req, res) => {
 app.put("/api/customer/:id", (req, res) => {
     const customer = req.body;
 
-    if (!customer.name || customer.email || customer.phone_no || customer.image || customer.description ) {
+    if (!customer.name || !customer.email || !customer.phone_no || !customer.image || !customer.description ) {
         return res.status(400).json({ error: "Invalid payload" });
     }
 
     pool.query(
-        "UPDATE INTO customer SET name =?, email =?, phone_id =?, image =?, description =? WHERE id =?",
+        "UPDATE INTO customer SET name =?, email =?, phone_id =?, image =?, description =? WHERE id = ?",
         [customer.name, customer.email, customer.phone_no, customer.image, customer.description, req.params.id],
         (error, results) => {
             if (error) {
