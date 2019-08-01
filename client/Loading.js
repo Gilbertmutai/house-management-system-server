71a4a9dc2d6dc368b8fc15d0aeef4003220d9c2c
Kibetmutai <kibetmutaigil738@gmail.com>
2019-08-01 15:12:49 +0300

    add loading and error

---
+++ client/Loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loading() {
+    return (
+        <div className="hsls-fullpage-text">
+            <p>Loading...</p>
+        </div>
+    );
+}
+
+export default Loading;
