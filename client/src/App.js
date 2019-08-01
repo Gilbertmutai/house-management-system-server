59ab899ef9fdacd11530c4d181b767087e86c1f3
Kibetmutai <kibetmutaigil738@gmail.com>
2019-08-01 09:02:23 +0300

    add react-scripts to package.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
+import NavBar from "./NavBar";
 import HouseList from "./HouseList.js";
 import "./App.css";
-
-
 
 
 function App(House) {
   return (
     <div className="hsls-app">
       <header className="hsls-header">
+      <NavBar />
       </header>
       <main className="hsls-main">
-        <HouseList houseRenting={House} />
+        <HouseList houseShowing={House} />
       </main>
     </div>
   );
